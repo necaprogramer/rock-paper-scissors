59885180c72a16b5176caa15f0a5f508c82e8887
necaprogramer <nissimbara@outlook.com>
2022-06-30 20:03:18 +0200

    Add a function for playing rounds and info divs in HTML

--- js-resources/script.js
+++ js-resources/script.js
@@ -6,20 +6,35 @@
     The desired output is to register and store user's option inside of a variable and then compare it against the computer's randomised option. If the user's option compared to the computer's results in a win, then that win will be added to the user's scoreboard, and vice-versa. The user, and the computer can play 'till the fifth win of either or, and whoever wins the fifth game is the overall winner of the game.
 
     Given your inputs, what are the steps necessary to return the desired output?:
-        1. User is given a message, enter your option:
-        2. The user must enter either rock, paper or scissors, regardless of the case (it can be upper-case, lower-case, or mix of the both)
-        3. Once the user has entered his option it is stored in a string variable with the name userOption
-        4. Computer's option will be randomised between rock, paper or scissors with the help of randomised number (eg. if random number is 1 then the computer's choice equals to rock, 2 equals to paper, and 3 equals to scissors)
-        5. The options between the user and the computer will be compared:
-            - if user's option is rock, and computer's option is paper - computer wins
-            - if user's option is paper, and computer's option is scissors - computer wins
-            - if user's option is scissors, and computer's option is rock - computer wins
-            - if the options between the user and the computer are the same - it's a draw
-            - else - user wins
-        6. If the user wins then his win is stored inside of userScore integer variable
-        7. If the computer wins then its win is stored inside of computerScore integer variable
-        8. The number of games is increased with each game played
-        9. Once userScore, or computerScore reaches five, the game is ended
+    //FIRST VERSION
+        // 1. User is given a message, enter your option:
+        // 2. The user must enter either rock, paper or scissors, regardless of the case (it can be upper-case, lower-case, or mix of the both)
+        // 3. Once the user has entered his option it is stored in a string variable with the name userOption
+        // 4. Computer's option will be randomised between rock, paper or scissors with the help of randomised number (eg. if random number is 1 then the computer's choice equals to rock, 2 equals to paper, and 3 equals to scissors)
+        // 5. The options between the user and the computer will be compared:
+        //     - if user's option is rock, and computer's option is paper - computer wins
+        //     - if user's option is paper, and computer's option is scissors - computer wins
+        //     - if user's option is scissors, and computer's option is rock - computer wins
+        //     - if the options between the user and the computer are the same - it's a draw
+        //     - else - user wins
+        // 6. If the user wins then his win is stored inside of userScore integer variable
+        // 7. If the computer wins then its win is stored inside of computerScore integer variable
+        // 8. The number of games is increased with each game played
+        // 9. Once userScore, or computerScore reaches five, the game is ended
+
+    //SECOND VERSION
+        1. User is displayed a message -> "In order to play rock-paper-scissors you must choose one of the options below"
+        2. User is presented with three buttons with icons for rock, paper and scissors
+        3. Once the user click one of those buttons,  the user option will be stored in a string variable with the name userOption and the round is played
+        4. Computer's option is randomised between rock, paper or scissors with the help of a randomised number
+        5. The options between the user and the computer are compared
+        7. If the user wins then the win is stored inside of userScore integer variable
+        8. If the computer wins then te win is stored insided of the computerScore integer variable
+        9. The number of games is increased with each game played
+        10. Once userScore, or computerScore variable reaches integer number five, the game is ended
+        11. The winner of the game is announced with a separate div
+        12. If the user is the winner the div will contain a message -> "Oh thou, user you are truly mighty!"
+        13. If the computer is the winner the div will contain a message -> "MUHAHAHA! You really thought you could beat a computer!?"
 */
 
 
@@ -29,17 +44,24 @@ let drawCount = 0;
 let userWonCount = 0;
 let computerWonCount = 0;
 
-function userOption(){
+playRound();
+
+function messageDisplay(){
+    
+}
+
+function playRound(){
     let rock = document.getElementById('rock');
     let paper = document.getElementById('paper');
     let scissors = document.getElementById('scissors');
 
-    
+    rock.addEventListener('click', () => {accessingScore('rock', generationOfComputerOption())});
+    paper.addEventListener('click', () => {accessingScore('paper', generationOfComputerOption())});
+    scissors.addEventListener('click', () => {accessingScore('scissors', generationOfComputerOption())});
 }
 
 function generationOfComputerOption(){
     let optionGenerated = Math.floor((Math.random() * 11));
-    //console.log(option);
     if(optionGenerated <= 3){
         let weaponIsRock = 'rock';
         return weaponIsRock;
@@ -65,20 +87,5 @@ function accessingScore(userOption, computerOption){
         return drawCount += 1;
     }else{
         return userWonCount += 1;
-    }
-}
-
-function playingGame(){
-    while((userWonCount < 5) && (computerWonCount < 5)){
-        
-        let computerOption = generationOfComputerOption();
-        console.log(computerOption);
-
-        accessingScore(userOption, computerOption);
-    }
-    if(userWonCount == 5){
-        alert('Congradulations! You are truly mighty user!');
-    }else if(computerWonCount == 5){
-        alert('Huh, did you really think you could beat a computer!? MUHAHAHAHA!');
     }
 }
